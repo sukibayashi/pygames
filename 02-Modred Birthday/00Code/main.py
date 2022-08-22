2e5ddacb348505893e277d1738857f9a59fd9372
sukibayashi <tony9033bin@gmail.com>
2022-08-22 15:50:18 +0800

    2022/08/22 update

---
+++ 02-Modred Birthday/00Code/main.py
@@ -0,0 +1,32 @@
+import pygame, sys
+from settings import * # 導入遊戲設定
+from level import Level
+from debug import debug
+
+class Game:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT)) # 控制熒幕大小
+        pygame.display.set_caption('Modred Birthday') # 標題
+        self.clock = pygame.time.Clock() # 控制FPS
+
+        self.level = Level() # 建立關卡
+ 
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:   # 當遊戲關閉時
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')   # 填充黑色
+            self.level.run() # 更新關卡
+            pygame.display.update() # 刷新
+            self.clock.tick(FPS)    # 控制FPS
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+
